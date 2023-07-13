4817a1336648ae1f60d0db13a99f4493c228fc22
Weather forecast with Alan AI <niveditamahato2001@gmail.com>
2023-07-13 13:38:17 +0530

    About Us Section added

--- components/aboutus/AboutUs.js
+++ components/aboutus/AboutUs.js
@@ -1,5 +1,45 @@
 import React from "react";
-
+import styles from "./AboutUs.module.scss";
+import Image from "next/image";
+import Title3 from "../imgs/Title3.png"
+import SubImage1 from "../imgs/SubImage1.png";
+import SubImage2 from "../imgs/SubImage2.png";
+import SubTitle1 from "../imgs/SubTitle1.png";
+import SubTitle2 from "../imgs/SubTitle2.png"
 export default function AboutUs() {
-  return <div>AboutUs</div>;
+  return(
+    <>
+    <div className={styles.AboutUs} >
+      <div className={styles.Title3} >
+        <Image src={Title3} alt="Title 3"  />
+      </div>
+      <div className={styles.AboutUsSection} >
+        <div className={styles.SubImage}>
+
+        <Image src={SubImage1} alt="Sub Image 1" width={500} />
+        </div>
+        <div className={styles. RightSection} >
+          <Image src={SubTitle1} alt="Sub Title" width={400} className={styles.SubTitle1} />
+          <div className={styles.rightContent } >
+          1/3rd of food produced for human consumption is wasted globally, which accounts for almost 1.3 billion tons per year. On the other hand, as per WHO 20% of the population faces extreme food shortages.
+          </div>
+        </div>
+      </div>
+      <div className={styles.AboutUsSection} >
+        <div className={styles.LeftSection} >
+          <Image src={SubTitle2} alt="Sub Title" width={400} className={styles.SubTitle2} />
+          <div className={styles.LeftContent} >
+          The act of donating food is not limited to large-scale contributions. Even a small donation can have a ripple effect, inspiring others to join the cause and multiply the impact. It fosters a sense of compassion and unity within our communities, reminding us of the power we possess to uplift others and create positive change.
+          </div>
+        </div>
+        <div className={styles.SubImage} >
+          <Image src={SubImage2} alt="Sub Image" width={500}  />
+        </div>
+      </div>
+      <div className={styles.Comments} >
+        <div className={styles.SubTitle}>Hear why our community loves Petaraa Annapurna   </div>
+      </div>
+    </div>
+    </>
+  );
 }
--- components/mission/Mission.js
+++ components/mission/Mission.js
@@ -1,5 +1,29 @@
 import React from "react";
+import styles from "./Mission.module.scss";
+import Image from "next/image";
+import Title2 from "../imgs/Title2.png";
+import OurMission from "../imgs/OurMission.png"
 
 export default function Mission() {
-  return <div>Mission</div>;
+  return (
+    <>
+    <div className={styles.OurMission} >
+      <div className={styles.Mission} >
+        
+          <Image src={Title2} alt="Title 2" width={300} />
+          <div className={styles.MissionSection} >
+          <div className={styles.MissionContent} >Analyze and reduce the food wastage from restaurants and hotels.</div>
+ <div className={styles.MissionContent} >Connect people looking for a way to donate  without wasting excess food.</div>
+ 
+ <div className={styles.MissionContent} >'Annapurna' is a cross-platform application that facilitates food donation to the nearest Charity. </div>
+ 
+ 
+        </div>
+      </div>
+      <div className={styles.MissionImg} >
+        <Image src={OurMission} alt="Our Mission" width={600} />
+      </div>
+    </div>
+    </>
+  );
 }
